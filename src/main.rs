2472ed62850c89081a11aa90041375bf9252791c
rshi1973 <rshi1973@hotmail.com>
2024-05-21 08:53:27 +0800

    error handling using anyhow and thiserror.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use ecosystem::MyError;
+
+fn main() -> Result<(), MyError> {
+    println!("size of MyError is {}", std::mem::size_of::<MyError>());
+
+    // let filename = "non_existent_file.txt";
+    // let fd = fs::File::open(filename).with_context(|| format!("file: {}", filename))?;
+
+    fail_with_error()?;
+
+    Ok(())
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("An error message".to_string()))
 }
